8fea5ef699a377eb617b5f4be97c57cb40b97182
Mr-Jess <jesse.passeri@codeimmersives.com>
2019-10-01 15:41:24 -0400

    completed

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 function fizzy(num) {
-
+  if (num % 3 === 0 && !(num % 5===0)) {
+return 'Fizz';
+} else if (num % 5 ===0 && !(num % 3===0)) {
+  return 'Buzz';
+} else if (num % 15 ===0) {
+  return 'FizzBuzz';
+} else {
+  return num;
 }
+}
+console.log(fizzy(150))
+
+
 
 module.exports = {
   fizzy,
